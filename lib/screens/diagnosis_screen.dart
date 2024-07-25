521940ad07b1a47ad57b7682c7434f547f9da1d6
minseo <vhehddl2212@naver.com>
2024-07-25 11:49:31 +0900

    revise diagnosis screen

--- lib/screens/diagnosis_screen.dart
+++ lib/screens/diagnosis_screen.dart
@@ -11,22 +11,19 @@ class _DiagnosisScreenState extends State<DiagnosisScreen> {
   final String question = '나와 어울리는 색을 선택해주세요';
 
   final List<List<Color>> options = [
-    // 여름 쿨톤 색상
+
     [Colors.lightBlue[100]!, Colors.deepPurple[100]!],
     [Colors.blue[100]!, Colors.purple[100]!],
     [Colors.cyan[200]!, Colors.teal[100]!],
 
-    // 겨울 쿨톤 색상
     [Colors.blue[800]!, Colors.purple[800]!],
     [Colors.indigo[600]!, Colors.blueGrey[700]!],
     [Colors.blueGrey[300]!, Colors.cyan[700]!],
 
-    // 봄 웜톤 색상
     [Colors.yellowAccent[100]!, Colors.pink[100]!],
     [Colors.orange[200]!, Colors.yellow[200]!],
     [Colors.amber[200]!, Colors.orange[300]!],
 
-    // 가을 웜톤 색상
     [Colors.orange[800]!, Colors.yellow[800]!],
     [Colors.brown[700]!, Colors.deepOrange[700]!],
     [Colors.red[400]!, Colors.brown[300]!],
@@ -39,6 +36,7 @@ class _DiagnosisScreenState extends State<DiagnosisScreen> {
     [Colors.red[300]!, Colors.redAccent[200]!],
     [Colors.pink[300]!, Colors.deepOrange[300]!],
   ];
+
   int currentQuestionIndex = 0;
   List<int> selectedColorIndices = [];
   String result = '';
