3d760a71ddb614efca69a47c7b7f2676a28b4883
minseo <vhehddl2212@naver.com>
2024-09-19 00:44:56 +0900

    add weather category

--- lib/screens/recommendation_screen.dart
+++ lib/screens/recommendation_screen.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:myminicloset/imagerepository.dart';
-import 'package:myminicloset/provider/userprovider.dart';
-import 'package:provider/provider.dart';
 
 class RecommendationScreen extends StatefulWidget {
   @override
@@ -10,46 +6,30 @@ class RecommendationScreen extends StatefulWidget {
 }
 
 class _RecommendationScreenState extends State<RecommendationScreen> {
-  final ImageRepository _imageRepository = ImageRepository();
-  List<Map<String, dynamic>> _uploadedImages = [];
-  List<Map<String, dynamic>> _selectedImages = [];
+  String? selectedCategory;
+  String? selectedSeason;
   bool _isLoading = false;
-  late String userId;
-  @override
-  void initState() {
-    super.initState();
-    userId = Provider.of<UserProvider>(context, listen: false).userId;
-    _fetchImages();
-  }
 
-  Future<void> _fetchImages() async {
-    //String userId = '1234'; // 추후 로그인 기능 구현 시 userId를 사용하세요.
-    try {
-      List<Map<String, dynamic>> imagesData = await _imageRepository.getImages(userId);
-      setState(() {
-        _uploadedImages = imagesData;
-      });
-    } catch (e) {
-      print('Error fetching images: $e');
-    }
-  }
+  final Map<String, List<String>> styleCategories = {
+    "클래식": ["클래식", "프레피"],
+    "매니시": ["매니시", "톰보이"],
+    "엘레강스": ["엘레강스", "소피스케이티드", "글래머러스"],
+    "에스닉": ["에스닉", "히피", "오리엔탈"],
+    "모던": ["모던", "미니멀"],
+    "내추럴": ["내추럴", "컨트리", "리조트"],
+    "로맨틱": ["로맨틱", "섹시"],
+    "스포티": ["스포티", "애슬레져", "밀리터리"],
+    "문화": ["뉴트로", "힙합", "키티/키덜트", "맥시멈", "펑크/로커"],
+    "캐주얼": ["캐주얼", "놈코어"]
+  };
 
-  void _toggleSelection(Map<String, dynamic> image) {
-    setState(() {
-      if (_selectedImages.contains(image)) {
-        _selectedImages.remove(image);
-      } else {
-        _selectedImages.add(image);
-      }
-    });
-  }
+  final List<String> seasons = ["봄", "여름", "가을", "겨울"];
 
   void _generateOutfit() async {
     setState(() {
       _isLoading = true;
     });
 
-    // AI 코디 생성 로직 추가해야됨..
     await Future.delayed(Duration(seconds: 2));
 
     setState(() {
@@ -57,11 +37,50 @@ class _RecommendationScreenState extends State<RecommendationScreen> {
     });
   }
 
+  Widget _buildSeasonButton(String season) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            selectedSeason = season;
+          });
+        },
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 15),
+          decoration: BoxDecoration(
+            color: selectedSeason == season ? Colors.blueAccent : Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 3,
+                blurRadius: 5,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Center(
+            child: Text(
+              season,
+              style: TextStyle(
+                color: selectedSeason == season ? Colors.white : Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('나만의 옷장으로 코디하기', style: TextStyle(color: Colors.black)),
+        title: Text(
+          'AI 코디 생성기',
+          style: TextStyle(color: Colors.black),
+        ),
         backgroundColor: Colors.white,
         elevation: 0,
         iconTheme: IconThemeData(color: Colors.black),
@@ -72,110 +91,86 @@ class _RecommendationScreenState extends State<RecommendationScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              '나만의 옷장으로 AI가 추천하는 코디를 받아보세요!',
+              'AI가 추천하는 코디를 받아보세요!',
               style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
             Container(
-              padding: EdgeInsets.all(16.0),
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
               decoration: BoxDecoration(
-                color: Colors.yellow[100],
-                borderRadius: BorderRadius.circular(10.0),
-                border: Border.all(color: Colors.yellow, width: 2),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '장바구니',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 10),
-                  Container(
-                    height: 120,
-                    child: _selectedImages.isEmpty
-                        ? Center(child: Text('선택된 사진이 없습니다. 옷을 담아보세요!'))
-                        : SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: _selectedImages.map((image) {
-                          return Padding(
-                            padding: const EdgeInsets.only(right: 10.0),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10.0),
-                              child: CachedNetworkImage(
-                                imageUrl: image['image'],
-                                width: 100,
-                                height: 100,
-                                fit: BoxFit.cover,
-                                placeholder: (context, url) => CircularProgressIndicator(),
-                                errorWidget: (context, url, error) => Icon(Icons.error),
-                              ),
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                    ),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 3,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
                   ),
                 ],
               ),
+              child: DropdownButton<String>(
+                value: selectedCategory,
+                hint: Text('카테고리 선택'),
+                isExpanded: true,
+                underline: SizedBox(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedCategory = newValue;
+                  });
+                },
+                items: styleCategories.keys.map((String category) {
+                  return DropdownMenuItem<String>(
+                    value: category,
+                    child: Text(category),
+                  );
+                }).toList(),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: seasons.map((season) => _buildSeasonButton(season)).toList(),
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: _generateOutfit,
               child: Text(
-                'AI 코디 생성하기',
-                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                'AI 코디 생성하기!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 23,
+                ),
               ),
               style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
+                backgroundColor: Colors.pinkAccent,
                 minimumSize: Size(double.infinity, 60),
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
+                  borderRadius: BorderRadius.circular(30.0),
                 ),
                 elevation: 5,
               ),
             ),
             SizedBox(height: 20),
             Expanded(
-              child: _isLoading
-                  ? Center(child: CircularProgressIndicator())
-                  : _uploadedImages.isEmpty
-                  ? Center(child: Text('업로드된 옷이 없습니다.'))
-                  : GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                ),
-                itemCount: _uploadedImages.length,
-                itemBuilder: (context, index) {
-                  final image = _uploadedImages[index];
-                  final isSelected = _selectedImages.contains(image);
-
-                  return GestureDetector(
-                    onTap: () => _toggleSelection(image),
-                    child: Stack(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(10.0),
-                          child: CachedNetworkImage(
-                            imageUrl: image['image'],
-                            placeholder: (context, url) => CircularProgressIndicator(),
-                            errorWidget: (context, url, error) => Icon(Icons.error),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        if (isSelected)
-                          Positioned(
-                            top: 8,
-                            right: 8,
-                            child: Icon(Icons.check_circle, color: Colors.green, size: 30),
-                          ),
-                      ],
+              child: Center(
+                child: _isLoading
+                    ? CircularProgressIndicator()
+                    : Container(
+                  width: double.infinity,
+                  height: 500,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '코디 화면 (대체 이미지)',
+                      style: TextStyle(fontSize: 18, color: Colors.black54),
                     ),
-                  );
-                },
+                  ),
+                ),
               ),
             ),
           ],
